83bd2d7fcbffaf59dc99c62aeb235fda03deed84
Abimael Vieira Borges <abimaelvieirawelwel@gmail.com>
2025-07-23 09:36:04 -0300

    adding classes generated from staruml

---
+++ code/game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window = None
+        self.Port1 = None
+        self.name = None
+        self.enity_list = None
+
+    def Operation1(self, ):
+        pass
+
+    def run(self, ):
+        pass
