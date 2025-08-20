7e0a706978733d4796b77262ad5b2a395d1ea86d
Abimael Vieira Borges <abimaelvieirawelwel@gmail.com>
2025-08-20 01:54:07 -0300

    Creating player shot Creating Enemy shot

--- code/Const.py
+++ code/Const.py
@@ -11,14 +11,22 @@ MENU_OPTIONS = ('NEW GAME 1P',
 
 )
 EVENT_ENEMY = pygame.USEREVENT + 1
+
 ENTITY_SPEED = {
     'level10': 0,
     'level11': 0.01,
     'level12': 1,
     'level13': 2,
     'level14': 3,
+
     'nave_1': 2,
     'nave_2': 1.5,
+
+    'nave_1_shot': 3,
+    'nave_2_shot': 4,
+
+    'ship_pixel_player1_shot': 3,
+    'ship_pixel_player2_shot': 3,
 }
 ENTITY_HEALTH = {
     'level10':999,
@@ -32,16 +40,30 @@ ENTITY_HEALTH = {
 
     'nave_1': 55,
     'nave_2':65,
+
+    'ship_pixel_player1_shot':1,
+    'nave_1_shot':1,
+    'nave_2_shot':2,
+}
+ENITY_SHOT_DELAY ={
+    'ship_pixel_player1':13,
+    'ship_pixel_player2':13,
+    'nave_1':16,
+    'nave_2':16,
 }
 
 PLAYER_KEY_UP = {'ship_pixel_player1':pygame.K_UP,
                 'ship_pixel_player2': pygame.K_w}
+
 PLAYER_KEY_DOWN={'ship_pixel_player1':pygame.K_DOWN,
                 'ship_pixel_player2':pygame.K_s}
+
 PLAYER_KEY_LEFT={'ship_pixel_player1':pygame.K_LEFT,
                 'ship_pixel_player2':pygame.K_a}
+
 PLAYER_KEY_RIGHT={'ship_pixel_player1':pygame.K_RIGHT,
                  'ship_pixel_player2':pygame.K_d}
+
 PLAYER_KEY_SHOOT={'ship_pixel_player1':pygame.K_RCTRL,
                  'ship_pixel_player2':pygame.K_LCTRL}
 
--- code/Player.py
+++ code/Player.py
@@ -4,14 +4,16 @@ import pygame.key
 import pygame
 from pygame import joystick
 
-from code.Const import PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT
+from code.Const import PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, \
+    ENITY_SHOT_DELAY
 from code.Entity import Entity
-
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENITY_SHOT_DELAY[self.name]
 
 
     def move(self, ):
@@ -25,6 +27,12 @@ class Player(Entity):
             self.rect.right -= 2
         if press_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.left < 525:
             self.rect.right += 3
+        pass
 
-        else:
-            pass
+    def shoot(self, ):
+        self.shot_delay-=1
+        if self.shot_delay ==0:
+            self.shot_delay =ENITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(name = f'{self.name}_shot', position=(self.rect.centerx, self.rect.centery))
